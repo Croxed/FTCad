64fe4cfb0bf1f1718d3f75b6c0ac5bfc407574bc
Carl Andersson <me@moofius.com>
2017-02-13 13:04:42 +0100

    Cleaned up common message things

--- src/common/Message.java
+++ src/common/Message.java
@@ -1,7 +1,5 @@
 package common;
 
-import java.io.*;
 
 public interface Message {
-	public void serializeToStream(DataOutputStream out) throws IOException;
 }
--- src/common/PingMessage.java
+++ src/common/PingMessage.java
@@ -3,14 +3,7 @@ package common;
 import java.io.*;
 
 public class PingMessage implements Message {
-	public static final int typeID = 1;
-
 	public PingMessage() {
 		
-	}
-
-	@Override
-	public void serializeToStream(DataOutputStream out) throws IOException {
-		out.writeInt(typeID);
 	}
 }
