556bd7b8c6ca077f4c4524fbdec301014f394410
Carl Andersson <me@moofius.com>
2017-02-15 15:48:01 +0100

    Added ping to server to frontend communicator

--- src/server/FrontEndCommunicator.java
+++ src/server/FrontEndCommunicator.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 
 import common.ServerWithFrontEnd.ConnectionRespondMessage;
+import common.PingMessage;
 import common.ServerWithFrontEnd.ConnectionRequestMessage;
 
 public class FrontEndCommunicator extends Thread {
@@ -16,6 +17,9 @@ public class FrontEndCommunicator extends Thread {
 	
 	private Boolean primary;
 
+	/**
+	 * Connects and listens to frontend 
+	 */
 	public FrontEndCommunicator(Server _server, String _hostName, int _hostPort) {
 		server = _server;
 		hostName = _hostName;
@@ -26,20 +30,34 @@ public class FrontEndCommunicator extends Thread {
 		return (socket != null && socket.isConnected());
 	}
 	
-	
+
+	/**
+	 * Connects and listens to frontend 
+	 */
 	public void run() {
 		while (true) {
 			try {
 				System.out.println("Trying to send server request message");
 				socket = new Socket(java.net.InetAddress.getByName(hostName), hostPort);
+				socket.setSoTimeout(5000);
 				input = new ObjectInputStream(socket.getInputStream());
 				output = new ObjectOutputStream(socket.getOutputStream());
-				
 			
 				output.writeObject(new ConnectionRequestMessage(server.getPort()));
 				System.out.println("Sent server request message");
 				
+				Pinger ping = new Pinger();
+				ping.start();
+				
 				listenToFrontEnd();
+				try {
+					ping.interrupt();
+					ping.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				socket.close();
+				
 			} catch (IOException e) {
 				System.out.println("Connection refused. Trying again in 1 second");
 				try { Thread.sleep(1000); } catch (InterruptedException sleep) { }
@@ -70,5 +88,18 @@ public class FrontEndCommunicator extends Thread {
 	
 	public Boolean getType() {
 		return primary;
+	}
+	
+	private class Pinger extends Thread {
+		public void run() {
+			while (!Thread.currentThread().isInterrupted()) {
+				try {
+					output.writeObject(new PingMessage());
+					try { Thread.sleep(1000); } catch (InterruptedException sleep) { }
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
