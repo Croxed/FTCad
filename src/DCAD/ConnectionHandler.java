3bc332b01f5a6b8c16118857b5c6bdc2fc26fc79
Carl Andersson <me@moofius.com>
2017-02-23 14:20:47 +0100

    Fixed stupid errors in connectionhandler, and some refractoring

--- src/DCAD/ConnectionHandler.java
+++ src/DCAD/ConnectionHandler.java
@@ -17,9 +17,6 @@ import common.ClientWithFrontEnd.ConnectionRespondMessage;
  */
 public class ConnectionHandler implements Runnable {
 
-	private Socket mSocket;
-	private ObjectOutputStream mOStream;
-	private ObjectInputStream mIStream;
 	private InetAddress mFrontEndAddress;
 	private int mFrontEndPort = -1;
 	private InetAddress mServerAddress;
@@ -48,7 +45,7 @@ public class ConnectionHandler implements Runnable {
 		while(true) {
 			mGUI.removeEvents();
 		
-			initializeConnections();
+			getMainServerFromFrontend();
 			
 			try {
 				mSc = new ServerConnection(mGUI, mServerAddress, mServerPort);
@@ -65,19 +62,39 @@ public class ConnectionHandler implements Runnable {
 	 * When connected, attempts to receive connection details from the front end. 
 	 * Then, disconnects from the front end and connects to the specified primary server. 
 	 */
-	private void initializeConnections() {
+	private void getMainServerFromFrontend() {
 		// Try and connect to the frontend   
 		while(true) {
 			System.out.println("Attempting to connect to Frontend");
 			try {
-				connectToFrontEnd();
+				Socket socket = new Socket(mFrontEndAddress, mFrontEndPort);
+				ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
 
 				System.out.println("Getting address information to the main server");
-				getAddressFromFrontEnd();
+				
+				Object response = null;
+				try {
+					output.writeObject(new ConnectionRequestMessage());
+					response = input.readObject();
+				} catch (IOException | ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+				
+				if(response instanceof ConnectionRespondMessage) {
+					ConnectionRespondMessage msg = (ConnectionRespondMessage)response;
+					// Set the address to the primary server
+					mServerAddress = msg.getAddress();
+					mServerPort = msg.getPort();
+					System.out.println("Will connect to primary server with IP: " + mServerAddress + " and port: " + mServerPort);
+					return;
+				} else {
+					System.out.println("Unknown text received from frontend");
+				}
 				
 				System.out.println("Disconnecting from Frontend");
 				try {
-					disconnectSocket();
+					socket.close();
 				} catch (IOException e) {
 					System.err.println("Could not disconnect from the Frontend");
 				}
@@ -108,48 +125,5 @@ public class ConnectionHandler implements Runnable {
 		mSc.write(createdGObject);
 	}
 
-	/**
-	 * Connects to the front end, in order to get the address to the main server.
-	 * @throws IOException if the socket or the streams cannot be opened.
-	 */
-	private void connectToFrontEnd() throws IOException {
-		mSocket = new Socket(mFrontEndAddress, mFrontEndPort);
-		mOStream = new ObjectOutputStream(mSocket.getOutputStream());
-		mIStream = new ObjectInputStream(mSocket.getInputStream());
-	}
-	
-	/**
-	 * Set the address and port that is needed to connect to the server. Needs to connect to the front end and disconnect after obtaining the info.
-	 */
-	private void getAddressFromFrontEnd() {
-		Object response = null;
-		try {
-			mOStream.writeObject(new ConnectionRequestMessage());
-			response = mIStream.readObject();
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		if(response instanceof ConnectionRespondMessage) {
-			ConnectionRespondMessage msg = (ConnectionRespondMessage)response;
-			// Set the address to the primary server
-			mServerAddress = msg.getAddress();
-			mServerPort = msg.getPort();
-			
-			System.out.println("Will connect to primary server with IP: " + mServerAddress + " and port: " + mServerPort);
-		} else {
-			// Got some unknown shit from the frontend probably, if ending up here!! 
-		}
-	}
-
-	/**
-	 * Disconnects the socket, whether is connected to the front end or the primary server.
-	 * @throws IOException if the streams cannot be closed, or if the socket cannot be closed. 
-	 */
-	private void disconnectSocket() throws IOException {
-		mOStream.close();
-		mIStream.close();
-		mSocket.close();
-	}
 	
 }
